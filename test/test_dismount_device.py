b09356cf52248b417bfd66062ca7b38a7bb5c591
Ken Jarrad <kjarrad@cisco.com>
2015-05-14 06:54:03 +0000

    Remove delay because mount/dismount are atomic.

--- test/test_dismount_device.py
+++ test/test_dismount_device.py
@@ -1,4 +1,4 @@
-# Copyright 2014 Cisco Systems, Inc.
+# Copyright 2015 Cisco Systems, Inc.
 # 
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
 # the License. You may obtain a copy of the License at
@@ -10,7 +10,7 @@
 # specific language governing permissions and limitations under the License.
 
 from __future__ import print_function
-from basics.inventory import device_dismount, inventory_mounted, inventory_unmounted, device_mount, mounted
+from basics.inventory import device_dismount, inventory_mounted, inventory_unmounted, device_mount, mounted, device_control
 from settings import config
 import time
 from unittest.case import TestCase
@@ -21,33 +21,34 @@ class Test(TestCase):
     network_device_config = config['network_device']
 
     def setUp(self):
+        """
+        Mount every device that is unmounted.
+        """
         unmounted_list = inventory_unmounted()
-        for device_name in unmounted_list:
-            device_config = self.network_device_config[device_name]
-            print('setup: device_mount(' + device_name, *device_config.values(), sep=', ', end=')\n')
-            device_mount(
-                device_name,
-                device_config['address'],
-                device_config['port'],
-                device_config['username'],
-                device_config['password'])
-        
         if unmounted_list:
-            print('Sleep to allow Controller to update...')
-            time.sleep(2)
-            self.assertTrue(inventory_mounted(), 'Not mounted: ' + str(inventory_unmounted()))
+            print('setup:')
+            for device_name in unmounted_list:
+                device_config = self.network_device_config[device_name]
+                print('\tdevice_mount(' + device_name, *device_config.values(), sep=', ', end=')\n')
+                device_mount(
+                    device_name,
+                    device_config['address'],
+                    device_config['port'],
+                    device_config['username'],
+                    device_config['password'])
+                self.assertTrue(mounted(device_name), 'Expected mounted: ' + device_name)
 
-    def test_dismount_device(self):
+    def test_device_dismount(self):
+        """
+        Apply function device_dismount to every device that is mounted.
+        """
         mounted_list = inventory_mounted()
         self.assertTrue(mounted_list, 'One or more devices must be mounted.')
+        print('test:')
         for device_name in mounted_list:
-            print('test: device_dismount(' + device_name, end=')\n')
+            print('\tdevice_dismount(', device_name, end=')\n')
             device_dismount(device_name)
-
-        print('Sleep to allow Controller to update...')
-        time.sleep(2)
-        for device_name in mounted_list:
-            self.assertFalse(mounted(device_name), 'Mounted: ' + device_name)
+            self.assertFalse(mounted(device_name), 'Expected not mounted: ' + device_name)
 
 if __name__ == '__main__':
     main()
