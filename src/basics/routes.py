bf1b2a8c6985004374bb01dc1f5105607bd61958
Ken Jarrad <kjarrad@cisco.com>
2015-05-25 18:16:47 +0000

    Fix bugs related to failure to detect existence of static route.
    Issue #15 (removed unwanted url encoding)
    Issue #24 (use JSON for dynamic request content)

--- src/basics/routes.py
+++ src/basics/routes.py
@@ -9,6 +9,7 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
+from __future__ import print_function
 from ipaddress import ip_network, _BaseNetwork
 from basics.odl_http import odl_http_get, odl_http_post, odl_http_delete
 from basics.inventory import capability_discovery
@@ -18,7 +19,7 @@ _bgp_url_suffix = 'operational/bgp-rib:bgp-rib/rib/example-bgp-rib/loc-rib/table
 
 _static_route_url_template = 'config/opendaylight-inventory:nodes/node/{node-id}/yang-ext:mount/Cisco-IOS-XR-ip-static-cfg:router-static/default-vrf/address-family/vrfipv4/vrf-unicast/vrf-prefixes'
 
-_static_route_uni_url_template = _static_route_url_template + '/vrf-prefix/{vrf-prefix}'
+_static_route_uni_url_template = _static_route_url_template + '/vrf-prefix/{ip-address}/{prefix-length}'
 
 capability_ns = 'http://cisco.com/ns/yang/'
 capability_name = 'Cisco-IOS-XR-ip-static-cfg'
@@ -83,7 +84,11 @@ def static_route_json(device_name, destination_network):
         Return None if not found.
     """
     assert isinstance(destination_network, _BaseNetwork)
-    url_params = {'node-id' : device_name, 'vrf-prefix' : destination_network}
+    url_params = {
+        'node-id' : device_name, 
+        'ip-address' : destination_network.network_address, 
+        'prefix-length' : destination_network.prefixlen
+    }
     response = odl_http_get(_static_route_uni_url_template, url_params, 'application/json', expected_status_code=[200, 404])
     if response.status_code == 404:
         return None
@@ -93,13 +98,18 @@ def static_route_json(device_name, destination_network):
 def static_route_exists(device_name, destination_network):
     """ Determine whether the specified 'static route' exists on the specified device. """
     assert isinstance(destination_network, _BaseNetwork)
-    url_params = {'node-id' : device_name, 'vrf-prefix' : destination_network}
+    url_params = {
+        'node-id' : device_name, 
+        'ip-address' : destination_network.network_address, 
+        'prefix-length' : destination_network.prefixlen
+    }
     response = odl_http_get(_static_route_uni_url_template, url_params, 'application/json', expected_status_code=[200, 404])
     return response.status_code == 200
 
 def static_route_list(device_name):
     """ List the destination network of all 'static routes' on the specified device."""
     route_list = static_route_json_all(device_name)
+    print(route_list)
     return [to_ip_network(route['prefix'], route['prefix-length']) for route in route_list]
 
 def static_route_delete(device_name, destination_network):
@@ -109,41 +119,40 @@ def static_route_delete(device_name, destination_network):
         An exception is raised if the static route does not exist on the device.
     """
     assert isinstance(destination_network, _BaseNetwork)
-    url_params = {'node-id' : device_name, 'vrf-prefix' : destination_network}
+    url_params = {
+        'node-id' : device_name, 
+        'ip-address' : destination_network.network_address, 
+        'prefix-length' : destination_network.prefixlen
+    }
     response = odl_http_delete(_static_route_uni_url_template, url_params, expected_status_code=[200, 500])
     if response.status_code != 200:
         raise Exception(_error_message(response.json()))
 
-_static_route_content_template = \
-'''{"Cisco-IOS-XR-ip-static-cfg:vrf-prefix": [
-        {
-            "prefix": "%s",
-            "prefix-length": %s,
-            "vrf-route": {
-                "vrf-next-hops": {
-                    "next-hop-address": [
-                        {
-                            "next-hop-address": "%s",
-                            "description": "%s"
-                        }
-                    ]
-                }
-            }
-        }
-    ]
-}'''
-
 def static_route_create(device_name, destination_network, next_hop_address, description=None):
     """ Create the specified 'static route' on the specified network device. """
-    if not description:
-        description = 'static route to %s via %s' % (destination_network , next_hop_address)
-    request_content = _static_route_content_template % (destination_network.network_address, destination_network.prefixlen, next_hop_address, description)
+    next_hop = {"next-hop-address" : str(next_hop_address)}
+    if description:
+        next_hop["description"] = description
+
+    request_content = {
+        "Cisco-IOS-XR-ip-static-cfg:vrf-prefix": [
+            {
+                "prefix" : str(destination_network.network_address),
+                "prefix-length": destination_network.prefixlen,
+                "vrf-route" : {
+                    "vrf-next-hops" : {
+                        "next-hop-address" : [next_hop]
+                    }
+                }
+            }
+        ]
+    }
     response = odl_http_post(_static_route_url_template, {'node-id' : device_name}, 'application/json', request_content, expected_status_code=[204, 409])
     if response.status_code == 409:
         try:
-            raise ValueError(response.json()['errors']['error'][1]['error-message'])
+            raise ValueError(response.json()['errors']['error'][0]['error-message'])
         except IndexError:
             pass
         except KeyError:
             pass
-        raise ValueError('Already exists: ' + description)
+        raise ValueError('Already exists: static route to destination network %s on device %s' % (destination_network, device_name))
