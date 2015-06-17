b5c87a566433a5dfc1ba43cf5cfc33b8f1a14c3f
Ken Jarrad <kjarrad@cisco.com>
2015-06-17 14:06:28 +0100

    Issue #71 capability matrix presentation and implementation.

--- src/learning_lab/02_capability_matrix.py
+++ src/learning_lab/02_capability_matrix.py
@@ -11,79 +11,86 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-''' 
-    Display a matrix of capability rows and device columns.
-    This is a WIP as we need a HTML table output to make this work visually. TODO
-'''
+""" 
+Display a matrix of capabilities and network devices.
+
+The matrix has a row for each capability discovered on the network.
+The matrix has a column for each network device.
+Where each row and column intersect, the presence of that capability, on that 
+device, is indicated by the appearance of the revision (of the capability) or 
+blank if incapable.
+
+When the network contains identical devices, the columns of the matrix are 
+identical (and therefore redundant). This is often the case in a demonstration.
+However, when different models and products of network devices are combined, 
+the matrix is a rich source of information. It provides a convenient and concise
+summary of a heterogeneous network.
+
+All the information in the matrix is obtained from a single (HTTP) request to 
+the Controller.
+"""
 
 from __future__ import print_function as _print_function
-# from basics.capability import capability
-from basics.inventory import  inventory_connected
-from basics.inventory import capability
-# from basics.capability import capability as capabilityx
-# from basics.inventory import  inventory_mounted
-import re
-from collections import OrderedDict
+from basics.inventory import capability_discovery
+from itertools import chain
+from basics.interpreter import sys_exit, EX_OK, EX_TEMPFAIL
+from basics.render import print_table
+from pydoc import render_doc as doc, plain
+from inspect import cleandoc
 
-_qualified_name = re.compile(r'\((.*)\)(.*)')
-_revision = re.compile(r'(.*)\?revision=(.*)')
-
-def capability_matrix(devices):
-    capability_ids = set()
-    capability_by_device = {}
-    for device_name in devices:
-        capability_revision_by_id = {}
-        for capability_entire in capability(device_name):
-            match = _qualified_name.match(capability_entire)
-            qualifier = match.group(1)
-            capability_name = match.group(2)
-            match = _revision.match(qualifier)
-            
-            # namespace
-            ns = match.group(1)
-            # truncate redundant information
-            ns = ns[:-len(capability_name)] if ns.endswith(capability_name) else ns
-            
-            revision = match.group(2)
-            capability_id = (capability_name, ns)
-            capability_ids.add(capability_id)
-            capability_revision_by_id[capability_id] = revision
-        capability_by_device[device_name] = capability_revision_by_id
+def demonstrate(discoveries):
+    # Structure of map is {capability-id : revision-by-device}
+    # where capability-id is (capability-name, capability-namespace)
+    # and revision-by-device is {device-name : capability-revision}.   
+    revision_by_capability = {}
+    device_names = set()
     
-    # The 2 dimensional 'matrix' data structure.
-    # Each capability is a 'key' with a vector as 'value'.         
-    device_by_capability = OrderedDict()
-    
-    # Initialise 'capability' dimension and set 'device' dimension to empty vector.      
-#     for capability_id in sorted(capability_ids):
-#         device_by_capability[capability_id] = []
+    # Visit each discovery once and collect the capability identifier as a key
+    # and the capability revision as a (nested) value.
+    # During this single pass of discoveries, collect device names too.
+    for discovered in discoveries:
+        capability_id = (discovered.capability.name, discovered.capability.namespace)
+        revision_by_device = revision_by_capability.get(capability_id, {})
+        if not revision_by_device:
+            revision_by_capability[capability_id] = revision_by_device
+        revision_by_device[discovered.device_name] = discovered.capability.revision
+        device_names.add(discovered.device_name)
         
-    # Populate 'device' dimension per capability with the revision of each device.   
-    for capability_id in capability_ids:
-        revision_per_device = []
-        for device in devices:
-            capability_revision_by_id = capability_by_device[device]
-            revision_per_device.append(capability_revision_by_id[capability_id] if capability_id in capability_revision_by_id else None)
-        device_by_capability[capability_id] = revision_per_device
-        
-    return device_by_capability
+    # Order the devices alphabetically by name.
+    # This makes the column order deterministic.              
+    device_names = sorted(device_names)
 
-devices = []
-matrix = {}
+    # Flatten the dict into a 2D table.     
+    # Structure of table is [row, ...]
+    # where row is (capability-name, capability-namespace, revision, ...)
+    # where revision, ... is ordered list of one revision per device.               
+    table = [
+        tuple(chain(capability_id, [revision_by_device.get(device_name, '') for device_name in device_names]))             
+        for (capability_id, revision_by_device) in revision_by_capability.items()
+    ]
+    
+    # Order the table by capability name.     
+    # This makes the row order deterministic.              
+    table.sort()
+    headers = tuple(chain(('capability-name', 'capability-namespace'), device_names))
+    print_table(table, headers=headers)
 
 def main():
-    global devices
-    devices = sorted(inventory_connected())
-    
-    global matrix
-    matrix = capability_matrix(devices)
-    
-#     print(devices)
-#     for capability_name in device_by_capability.keys():
-#         revision_list = device_by_capability[capability_name]
-#         print(capability_name, revision_list)
+    print(cleandoc(__doc__))
+    print()
+    print('capability_discovery()')
+    discoveries = capability_discovery()
+    if not discoveries:
+        print("There are no capable network devices. Demonstration cancelled.")
+        return EX_TEMPFAIL
+    demonstrate(discoveries)
+    return EX_OK
 
 if __name__ == "__main__":
-    main()
-    print('devices:', devices)
-    print (*matrix.items(), sep='\n')
+    try:
+        sys_exit(main())
+    finally:
+        print()
+        print('Function Reference:')
+        print(plain(doc(capability_discovery)))
+        
